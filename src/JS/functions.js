fba5d43aac66a02e6139fa8518c5fc27dbb4ae70
Adam <adambrikman@gmail.com>
2019-09-28 23:51:09 -0700

    update functions

--- src/JS/functions.js
+++
@@ -1,8 +0,0 @@
-exports.makeCopyBtnVisible = function() {
-  return (document.getElementsByClassName("copyBtn").style.visibility =
-    "visible");
-};
-
-exports = module.exports;
-
-// https://www.youtube.com/watch?v=07-_Ze8wvDw
